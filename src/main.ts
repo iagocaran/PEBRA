16988138dc9d14c2280e02587658289f287206e0
Iago Caran <iago.caran@gmail.com>
2020-04-10 00:56:25 +0200

    Install Axios

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import axios from 'axios'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
 
 new Vue({
   router,
