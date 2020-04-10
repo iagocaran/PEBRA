c935b226fbd2537b75b0fd8cc1e14342e34c5b8e
Iago Caran <iago.caran@gmail.com>
2020-04-10 22:07:11 +0200

    Remove HelloWord and implement navigation in App

--- src/App.vue
+++ src/App.vue
@@ -45,24 +45,21 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default Vue.extend({
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
-    //
   }),
 });
 </script>
