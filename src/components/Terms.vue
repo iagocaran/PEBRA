f51f0a730b91e15413a0684ab3eb7883a9f4cb19
Iago Caran <iago.caran@gmail.com>
2020-04-12 23:30:07 +0200

    Create Terms component

---
+++ src/components/Terms.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card>
+
+  </v-card>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'Terms',
+
+    data: () => ({
+    }),
+  })
+</script>
+
+<style scoped>
+
+</style>
