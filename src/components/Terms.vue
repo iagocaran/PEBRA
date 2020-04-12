cb01afb01e727b9abc7db562a6863d6afe28e754
Iago Caran <iago.caran@gmail.com>
2020-04-13 01:51:38 +0200

    Added content to Terms dialog

--- src/components/Terms.vue
+++ src/components/Terms.vue
@@ -1,6 +1,25 @@
 <template>
-  <v-card>
-
+  <v-card class="pa-6">
+<!-- Center image -->
+    <v-img class="mb-4 center" src="@/assets/apex.png" width="250px"></v-img>
+    <div>
+      <h2>Termo de Responsabilidade</h2>
+      <h3>Termo de responsabilidade de uso do site</h3>
+      <p>Ao acessar o site da EXPORTA BRASIL, você concorda automaticamente com os termos e condições de utilização dos recursos oferecidos e seu funcionamento descrito na Política de Privacidade.</p>
+      <h3>Privacidade</h3>
+      <p>Recomendamos a leitura de nossa Política de Privacidade antes da navegação pelo site.</p>
+      <h3>Acesso às áreas com login e senha</h3>
+      <p>Às áreas de acesso com login e senha são restritas a pessoas com autorização prévia. Cada usuário autorizado é responsável por seus dados (login e senha) e pela segurança dos mesmos. A EXPORTA BRASIL não se responsabiliza pelo uso indevido destes, por você e por terceiros.</p>
+      <h3>Direito à propriedade intelectual</h3>
+      <p>Marcas, nomes comerciais e conteúdos oferecidos de qualquer espécie que estão veiculados ao site da EXPORTA BRASIL são de propriedade da empresa e não podem ser reproduzidos sem prévia autorização e respectiva titularidade.</p>
+      <h3>Isenção de responsabilidade</h3>
+      <p>Buscamos manter todas as informações do site atualizadas. Porém, pode haver atraso ou uma falha. Assim, a EXPORTA BRASIL se isenta de qualquer responsabilidade pela exatidão e integridade das informações divulgadas. Sugerimos que você confirme os dados divulgados antes de tomar uma decisão com base em um dos conteúdos apresentados.</p>
+      <h3>Limitação de responsabilidade</h3>
+      <p>A EXPORTA BRASIL não poderá ser responsabilizada, em nenhum caso, por quaisquer danos diretos, indiretos, casuais e especiais relacionados ou derivados deste site ou de seu uso, ou de qualquer site ou recurso vinculado, informação referenciada ou acessada por meio deste site, ou por uso ou download, ou acesso a quaisquer materiais, informações, produtos e serviços, incluindo, sem limitação, qualquer perda de lucros, interrupção de negócios, economias perdidas, perda de programas ou outros dados. Esta exclusão e renúncia de responsabilidade se aplicam a todas as causas de ação.</p>
+      <p>A EXPORTA BRASIL se reserva o direito de alterar os termos e condições, bem como os conteúdos do site, a qualquer momento, sem aviso prévio. Se desejar esclarecer alguma dúvida relativa à privacidade do site www.EXPORTA BRASIL.com.br, por favor, entre em contato conosco pelo e-mail seguranca@EXPORTA BRASIL.com.br.</p>
+    </div>
+<!-- TODO: Add close behavior -->
+    <v-btn color="primary" block>Continuar</v-btn>
   </v-card>
 </template>
 
