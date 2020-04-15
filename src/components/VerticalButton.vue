8b53ad9612eb6556d6135320e8f39410786207e9
Iago Caran <iago.caran@gmail.com>
2020-04-15 15:57:07 +0200

    Create VerticalButton component

---
+++ src/components/VerticalButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-btn text class="text-center pa-1" style="height: auto;">
+    <div class="d-flex flex-column">
+      <v-icon class="mb-2" size="32" color="light-blue darken-3">{{ icon }}</v-icon>
+      <span class="light-blue--text text--darken-3"><slot></slot></span>
+    </div>
+  </v-btn>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'VerticalButton',
+    props: [
+      'icon'
+    ],
+    data: () => ({})
+  })
+</script>
