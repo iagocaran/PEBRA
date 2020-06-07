f086c39f638aeb4d0e28425bfc1dd8cbe223af69
Iago Caran <iago.caran@gmail.com>
2020-06-08 01:25:26 +0200

    Create modal for successful registration

---
+++ src/components/Register.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card class="pa-6">
+    <div>
+      <h2>Cadastro realizado com sucesso!</h2>
+      <p>Você receberá um e-mail para ativação de sua conta.</p>
+    </div>
+    <v-btn color="primary" block @click="close">Voltar para o login</v-btn>
+  </v-card>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'Terms',
+    data: () => ({
+    }),
+    methods: {
+      close() {
+        this.$emit('close', true)
+      }
+    }
+  })
+</script>
+
+<style scoped>
+
+</style>
