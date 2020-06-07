a28796eb93542d83cb8b52e766d07d1a1efc5719
Iago Caran <iago.caran@gmail.com>
2020-06-08 01:24:26 +0200

    Add form validation to Login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,46 +6,48 @@
       <div style="width: 60px"></div>
       <v-tab>Cadastrar</v-tab>
     </v-tabs>
-    <v-tabs-items v-model="tab">
-      <v-tab-item>
-        <v-card flat>
-          <v-text-field label="E-mail" required hide-details="auto" :rules="emailRules"></v-text-field>
-          <v-text-field label="Senha" type="password" hide-details="auto" :rules="passwordRules"></v-text-field>
-          <div class="text-right mb-2">
-            <v-btn text class="caption light-blue--text text--darken-3">Esqueceu sua senha?</v-btn>
-          </div>
-          <v-btn color="primary" block @click="close()">Entrar</v-btn>
-          <div class="line-center ma-4">
-            <span class="pa-2 text-uppercase font-weight-medium grey--text text--darken-1">Ou</span>
-          </div>
-          <div class="d-flex justify-space-around mb-4">
-            <v-btn text fab><v-icon size="50" color="grey darken-1">mdi-google</v-icon></v-btn>
-            <v-btn text fab><v-icon size="50" color="grey darken-1">mdi-linkedin</v-icon></v-btn>
-            <v-btn text fab><v-icon size="50" color="grey darken-1">mdi-microsoft</v-icon></v-btn>
-          </div>
-        </v-card>
-      </v-tab-item>
-      <v-tab-item>
-        <v-card flat>
-          <v-text-field label="E-mail" required hide-details="auto" :rules="emailRules"></v-text-field>
-          <v-text-field label="Senha" type="password" hide-details="auto" :rules="passwordRules"></v-text-field>
-          <v-checkbox v-model="terms" hide-details="true" class="mb-2" required>
-            <template v-slot:label>
-              Li e aceito os <v-btn text @click.stop="termsDialog = true" class="d-inline-block pa-1 light-blue--text text--darken-3" style="text-decoration: underline">Termos de Uso</v-btn>
-            </template>
-          </v-checkbox>
-          <v-btn color="primary" block @click="close()">Entrar</v-btn>
-          <div class="line-center ma-4">
-            <span class="pa-2 text-uppercase font-weight-medium grey--text text--darken-1">Ou</span>
-          </div>
-          <div class="d-flex justify-space-around mb-4">
-            <v-btn text fab><v-icon size="50" color="grey darken-1">mdi-google</v-icon></v-btn>
-            <v-btn text fab><v-icon size="50" color="grey darken-1">mdi-linkedin</v-icon></v-btn>
-            <v-btn text fab><v-icon size="50" color="grey darken-1">mdi-microsoft</v-icon></v-btn>
-          </div>
-        </v-card>
-      </v-tab-item>
-    </v-tabs-items>
+    <v-form v-model="valid">
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <v-card flat>
+            <v-text-field label="E-mail" v-model="email" required hide-details="auto" :rules="emailRules"></v-text-field>
+            <v-text-field label="Senha" v-model="password" type="password" hide-details="auto" :rules="passwordRules"></v-text-field>
+            <div class="text-right mb-2">
+              <v-btn text class="caption light-blue--text text--darken-3">Esqueceu sua senha?</v-btn>
+            </div>
+            <v-btn color="primary" block @click="login" :disabled="!valid">Entrar</v-btn>
+            <div class="line-center ma-4">
+              <span class="pa-2 text-uppercase font-weight-medium grey--text text--darken-1">Ou</span>
+            </div>
+            <div class="d-flex justify-space-around mb-4">
+              <v-btn text fab><v-icon size="50" color="grey darken-1">mdi-google</v-icon></v-btn>
+              <v-btn text fab><v-icon size="50" color="grey darken-1">mdi-linkedin</v-icon></v-btn>
+              <v-btn text fab><v-icon size="50" color="grey darken-1">mdi-microsoft</v-icon></v-btn>
+            </div>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card flat>
+            <v-text-field label="E-mail" v-model="email" required hide-details="auto" :rules="emailRules"></v-text-field>
+            <v-text-field label="Senha" v-model="password" type="password" hide-details="auto" :rules="passwordRules"></v-text-field>
+            <v-checkbox v-model="terms" hide-details="true" class="mb-2" required>
+              <template v-slot:label>
+                Li e aceito os <v-btn text @click.stop="termsDialog = true" class="d-inline-block pa-1 light-blue--text text--darken-3" style="text-decoration: underline">Termos de Uso</v-btn>
+              </template>
+            </v-checkbox>
+            <v-btn color="primary" block @click="register" :disabled="!(valid && terms)">Continuar</v-btn>
+            <div class="line-center ma-4">
+              <span class="pa-2 text-uppercase font-weight-medium grey--text text--darken-1">Ou</span>
+            </div>
+            <div class="d-flex justify-space-around mb-4">
+              <v-btn text fab><v-icon size="50" color="grey darken-1">mdi-google</v-icon></v-btn>
+              <v-btn text fab><v-icon size="50" color="grey darken-1">mdi-linkedin</v-icon></v-btn>
+              <v-btn text fab><v-icon size="50" color="grey darken-1">mdi-microsoft</v-icon></v-btn>
+            </div>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-form>
     <v-dialog v-model="termsDialog" max-width="800">
       <terms @close="termsDialog = false"></terms>
     </v-dialog>
@@ -72,7 +74,8 @@
       ],
       passwordRules: [
         v => !!v || 'Digite sua senha'
-      ]
+      ],
+      valid: false
     }),
     methods: {
       close() {
