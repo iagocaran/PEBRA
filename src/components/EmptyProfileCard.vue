8fa7e42af59e8bd8e67329e50a9eb0c3fe38a5e6
Iago Caran <iago.caran@gmail.com>
2020-06-13 22:35:26 +0200

    Add EmptyProfileCard component

---
+++ src/components/EmptyProfileCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card raised class="mb-2">
+    <v-card-title class="text-uppercase pb-1">Bem-vindo!</v-card-title>
+    <v-card-text class="text-left">
+      <p class="mb-1">Complete seu perfil para acessar nossos benefícios</p>
+      <p class="mb-1"><b>E-mail:</b> </p>
+      <p class="mb-1"><b>Situação do perfil:</b></p>
+      <v-progress-linear rounded color="green" :height="24" :value="0" class="mb-1">
+        <div class="font-weight-bold pa-2">0%</div>
+      </v-progress-linear>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'EmptyProfileCard',
+    props: [
+    ],
+    methods: {
+    },
+    data: () => ({})
+  })
+</script>
