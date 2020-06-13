822742b6baa84ef566df7b7dc2dc8faf5a44496f
Iago Caran <iago.caran@gmail.com>
2020-06-13 13:13:54 +0200

    Reorganize UserHome layout

--- src/views/UserHome.vue
+++ src/views/UserHome.vue
@@ -47,63 +47,78 @@
     <v-content>
       <v-container class="pt-0">
         <v-row>
-          <v-col cols="8" class="ma-0 pa-0">
-            <v-row class="text-center">
-              <v-col cols="12">
-                <v-card raised class="mb-3">
-                  <div class="d-flex flex-no-wrap">
-                    <v-avatar size="150" tile>
-                      <v-img src="@/assets/user-logo.png"></v-img>
-                    </v-avatar>
-                    <div>
-                      <v-card-title class="text-uppercase pb-1">Technology - Slogan your company</v-card-title>
-                      <v-card-text class="text-left">
-                        <p class="mb-1">CNPJ: 12.345.67/0001-01</p>
-                        <v-progress-linear rounded color="green" :height="24" :value="60" class="mb-1">
-                          <div class="font-weight-bold pa-2">60%</div>
-                        </v-progress-linear>
-                        <p class="mb-0">Complete seu perfil agora e desfrute dos benefícios fornecidos pela APEX</p>
-                      </v-card-text>
-                    </div>
-                  </div>
-                </v-card>
-                <v-card raised class="mb-3">
-                  <div class="d-flex justify-space-around">
-                    <vertical-button icon="mdi-briefcase">Meu Negócio</vertical-button>
-                    <vertical-button icon="mdi-cog">Ferramentas</vertical-button>
-                    <vertical-button icon="mdi-calendar">Eventos</vertical-button>
-                    <vertical-button icon="mdi-chart-line">Estudos</vertical-button>
-                    <vertical-button icon="mdi-account-group">Contato</vertical-button>
-                    <vertical-button icon="mdi-account">Perfil</vertical-button>
-                  </div>
-                </v-card>
-                <v-card raised class="mb-3">
-                  <!-- TODO: Setup subnavigation -->
-                  <v-card-title>Meu negócio</v-card-title>
-                  <v-card-text>Entre no sistema e aproveite todas as ferramentas que preparamos para você!</v-card-text>
-                  <v-card-actions>
-                    <v-btn outlined block>
-                      Entrar
-                    </v-btn>
-                  </v-card-actions>
+          <v-col offset="1" cols="2">
+            <v-card raised class="mb-2">
+              <div class="text-center">
+                <v-img src="@/assets/user-logo.png" class="text-center"></v-img>
+              </div>
+              <v-card-title class="text-uppercase pb-1">Technology - Slogan your company</v-card-title>
+              <v-card-text class="text-left">
+                <p class="mb-1">CNPJ: 12.345.67/0001-01</p>
+                <v-progress-linear rounded color="green" :height="24" :value="60" class="mb-1">
+                  <div class="font-weight-bold pa-2">60%</div>
+                </v-progress-linear>
+                <p class="mb-0">Complete seu perfil agora e desfrute dos benefícios fornecidos pela APEX</p>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="8" class="pa-0">
+            <v-row>
+              <v-col cols="12" class="pb-0">
+                <v-bottom-navigation>
+                  <v-btn>
+                    <span>Início</span>
+                    <v-icon>mdi-road-variant</v-icon>
+                  </v-btn>
+                  <v-btn>
+                    <span>Meu Negócio</span>
+                    <v-icon>mdi-briefcase</v-icon>
+                  </v-btn>
+                  <v-btn>
+                    <span>Ferramentas</span>
+                    <v-icon>mdi-cog</v-icon>
+                  </v-btn>
+                  <v-btn>
+                    <span>Eventos</span>
+                    <v-icon>mdi-calendar</v-icon>
+                  </v-btn>
+                  <v-btn>
+                    <span>Estudos</span>
+                    <v-icon>mdi-chart-line</v-icon>
+                  </v-btn>
+                <v-btn>
+                  <span>Contato</span>
+                  <v-icon>mdi-account-group</v-icon>
+                </v-btn>
+                <v-btn>
+                  <span>Perfil</span>
+                  <v-icon>mdi-account</v-icon>
+                </v-btn>
+                </v-bottom-navigation>
+              </v-col>
+              <v-col>
+                <v-card raised>
+                  <v-card-title class="headline"><b>Meu Caminho Exportador</b></v-card-title>
+                  <v-row justify="space-between" class="pa-4 pt-0 pb-6">
+                    <v-col cols="5">
+                      <v-treeview dense :items="items" open-all>
+                        <template v-slot:prepend="{ item }">
+                          <v-icon v-if="item.type === 'file'" color="red">mdi-file-pdf</v-icon>
+                          <v-icon v-else-if="item.type === 'link'" color="blue">mdi-open-in-new</v-icon>
+                          <v-simple-checkbox color="success" v-model="item.check" v-else></v-simple-checkbox>
+                        </template>
+                      </v-treeview>
+                    </v-col>
+                    <v-divider vertical></v-divider>
+                    <v-col class="d-flex text-center" style="justify-content: center">
+                      <div class="title grey--text text--lighten-1 font-weight-light" style="align-self: center">
+                        Selecione para ver detalhes
+                      </div>
+                    </v-col>
+                  </v-row>
                 </v-card>
               </v-col>
             </v-row>
-          </v-col>
-          <v-col>
-            <!-- TODO: Turn into a component -->
-            <v-card raised>
-              <v-card-title>Meu Caminho Exportador</v-card-title>
-              <v-card-text>
-                <v-treeview dense :items="items" open-all>
-                  <template v-slot:prepend="{ item }">
-                    <v-icon v-if="item.type === 'file'" color="red">mdi-file-pdf</v-icon>
-                    <v-icon v-else-if="item.type === 'link'" color="blue">mdi-open-in-new</v-icon>
-                    <v-simple-checkbox color="success" v-model="item.check" v-else></v-simple-checkbox>
-                  </template>
-                </v-treeview>
-              </v-card-text>
-            </v-card>
           </v-col>
         </v-row>
       </v-container>
@@ -122,12 +137,10 @@
 
 <script lang="ts">
   import Vue from 'vue'
-  import VerticalButton from "@/components/VerticalButton.vue";
 
   export default Vue.extend({
     name: 'Home',
     components: {
-      VerticalButton
     },
     data: () => ({
       dialog: false,
