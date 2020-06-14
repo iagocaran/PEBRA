6f5fd41e5dc5927933d8ded2ef4a9ab9d0bd5f9e
Iago Caran <iago.caran@gmail.com>
2020-06-14 02:02:42 +0200

    Implement login logic and logged user endpoint to mockup server

--- src/server.js
+++ src/server.js
@@ -9,18 +9,29 @@ export function makeServer({ environment = "development" } = {}) {
     },
 
     seeds(server) {
-      server.create("user", { name: "Bob" })
-      server.create("user", { name: "Alice" })
+      server.create("user", { name: "Bob", email: "bob@mail.com", password: "1234" })
+      server.create("user", { name: "Alice", email: "a@a", password: "a", token: "qr8qp8ytb1o"})
     },
 
     routes() {
       this.namespace = "api"
 
-      this.post("/users/login", (schema) => {
+      this.post("/users/login", (schema, req) => {
         const now = new Date()
         const cookieExpiration = new Date(now.getTime() + 24 * 3600 * 1000)
-        document.cookie = `token=${Math.random().toString(36).substr(2)}; domain=localhost; path=/PEBRA; expires=${cookieExpiration.toUTCString()};`
-        return schema.users.find(1)
+        const credentials = JSON.parse(req.requestBody)
+        const user = schema.users.where({ email: credentials.email, password: credentials.password }).models[0]
+        // TODO: Verify if user exists, if not create it
+        // const token = Math.random().toString(36).substr(2)
+        const token = 'qr8qp8ytb1o'
+        document.cookie = `token=${token}; domain=localhost; path=/PEBRA; expires=${cookieExpiration.toUTCString()};`
+        return user.update({ token })
+      })
+
+      this.get("/users/me", (schema) => {
+        const token = document.cookie.split('; ').map((i) => i.split('=')).find((i) => i[0] === 'token')[1]
+        const user = schema.users.where({ token }).models[0]
+        return { email: user.email }
       })
 
       this.get("/users", (schema) => {
