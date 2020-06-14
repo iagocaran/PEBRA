d70deeabf8aa866fcdd65e3de8d48554cd6b50fc
Iago Caran <iago.caran@gmail.com>
2020-06-14 02:03:30 +0200

    Implement logged user data retrieval from server

--- src/components/EmptyProfileCard.vue
+++ src/components/EmptyProfileCard.vue
@@ -3,7 +3,7 @@
     <v-card-title class="text-uppercase pb-1">Bem-vindo!</v-card-title>
     <v-card-text class="text-left">
       <p class="mb-1">Complete seu perfil para acessar nossos benefícios</p>
-      <p class="mb-1"><b>E-mail:</b> </p>
+      <p class="mb-1"><b>E-mail:</b> {{ email }}</p>
       <p class="mb-1"><b>Situação do perfil:</b></p>
       <v-progress-linear rounded color="green" :height="24" :value="0" class="mb-1">
         <div class="font-weight-bold pa-2">0%</div>
@@ -17,10 +17,15 @@
 
   export default Vue.extend({
     name: 'EmptyProfileCard',
-    props: [
-    ],
     methods: {
     },
-    data: () => ({})
+    created () {
+      this.$http.get('/users/me', { baseURL: '/api' })
+        .then((res) => { this.email = res.data.email })
+        .catch((err) => { console.log(err) })
+    },
+    data: () => ({
+      email: ''
+    })
   })
 </script>
