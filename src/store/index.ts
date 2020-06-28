20027c929cfbafd6d2ff04079a2a4a4bd74c9b6b
Iago Caran <iago.caran@gmail.com>
2020-06-28 15:25:32 +0200

    Implement authenticated user data management

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,36 @@
 import Vue from 'vue'
+import http from 'axios'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: {}
   },
   mutations: {
+    updateUser (state, user) {
+      state.user = user
+    }
   },
   actions: {
+    updateUserData ({ commit }) {
+      return new Promise((resolve) => {
+        http.get('/users/me', { baseURL: '/api' })
+          .then((res) => {
+            commit('updateUser', res.data.user)
+          })
+          .catch((err) => { console.log(err) })
+          .finally(() => {
+            resolve()
+          })
+      })
+    }
+  },
+  getters: {
+    getUser (state) {
+      return state.user
+    }
   },
   modules: {
   }
