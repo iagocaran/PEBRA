2ac18cc96ccb727f0037892fa784e0d8f5347f82
Iago Caran <iago.caran@gmail.com>
2020-08-05 02:14:29 +0200

    Update UserHome template and functionality

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -3,11 +3,11 @@
     <div class="text-center">
       <v-img src="@/assets/user-logo.png" class="text-center"></v-img>
     </div>
-    <v-card-title class="text-uppercase pb-1">Technology - Slogan your company</v-card-title>
+    <v-card-title class="text-uppercase pb-1">{{ $store.getters.getUser.company }}</v-card-title>
     <v-card-text class="text-left">
-      <p class="mb-1">CNPJ: 12.345.67/0001-01</p>
-      <v-progress-linear rounded color="green" :height="24" :value="0" class="mb-1">
-        <div class="font-weight-bold pa-2">0%</div>
+      <p class="mb-1">E-mail: {{ $store.getters.getUser.email }}</p>
+      <v-progress-linear rounded color="green" :height="24" :value="15" class="mb-1">
+        <div class="font-weight-bold pa-2">15%</div>
       </v-progress-linear>
       <p class="mb-0">Complete seu perfil agora e desfrute de nossos benefícios</p>
     </v-card-text>
@@ -21,6 +21,8 @@
     name: 'ProfileCard',
     props: [
     ],
-    data: () => ({})
+    data: () => ({
+
+    })
   })
 </script>
